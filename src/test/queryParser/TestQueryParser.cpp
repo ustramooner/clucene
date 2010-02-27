e4969fd8deaa821c9508fa1ad3b4a6d17557fc83
Ben van Klinken <ustramooner@users.sourceforge.net>
2010-02-27 10:36:32 +0100

    fixed wildcard test - needed a delta

--- src/test/queryParser/TestQueryParser.cpp
+++ src/test/queryParser/TestQueryParser.cpp
@@ -370,11 +370,11 @@ void testWildcard(CuTest *tc)
 	assertCorrectQuery(tc, _T("t*"), NULL,"PrefixQuery", _T("t*"));
 
 	FuzzyQuery* fq = (FuzzyQuery*)getQuery(tc,_T("term~0.7"), NULL);
-	CuAssertTrue(tc, 0.7 == fq->getMinSimilarity()/*, 0.1*/);
+	CuAssertTrue(tc, abs(0.7 - fq->getMinSimilarity()) < 0.1);
 	CuAssertTrue(tc, FuzzyQuery::defaultPrefixLength == fq->getPrefixLength());
 	_CLLDELETE(fq);
 	fq = (FuzzyQuery*)getQuery(tc, _T("term~"), NULL);
-	CuAssertTrue(tc, 0.5 == fq->getMinSimilarity()/*, 0.1*/);
+	CuAssertTrue(tc, abs(0.5 - fq->getMinSimilarity()) < 0.1);
 	CuAssertTrue(tc, FuzzyQuery::defaultPrefixLength == fq->getPrefixLength());
 	_CLDELETE(fq);
 
