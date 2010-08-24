d67f4b2720c21460a63acdcbfa56f3324fb2fac8
Ben van Klinken <ustramooner@users.sourceforge.net>
2010-08-24 14:14:15 +0200

    fixed mistake in last commit

--- src/core/libclucene-core.pc.cmake
+++ src/core/libclucene-core.pc.cmake
@@ -6,6 +6,6 @@ includedir=${prefix}/include:${prefix}/include/CLucene/ext
 Name: libclucene
 Description: CLucene - a C++ search engine, ported from the popular Apache Lucene
 Version: @CLUCENE_VERSION_MAJOR@.@CLUCENE_VERSION_MINOR@.@CLUCENE_VERSION_REVISION@.@CLUCENE_VERSION_PATCH@
-Libs: -L${prefix}/lib/@LIB_DESTINATION@/ -lclucene-core
+Libs: -L${prefix}/@LIB_DESTINATION@/ -lclucene-core
 Cflags: -I${prefix}/include -I${prefix}/include/CLucene/ext
 ~
